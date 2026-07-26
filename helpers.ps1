e377474c7d76830999947095df14ce7708f2b4b3
brogers5 <6869577+brogers5@users.noreply.github.com>
2026-07-26 15:22:50 -0400

    Reformat

--- helpers.ps1
+++ helpers.ps1
@@ -4,47 +4,40 @@ $installerFileNameRegex = 'green-tunnel-windows.zip$'
 $owner = 'SadeghHayeri'
 $repository = 'GreenTunnel'
 
-function Get-LatestStableVersion
-{
+function Get-LatestStableVersion {
     $latestRelease = (Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Latest)[0]
 
     return [Version] $latestRelease.tag_name.Substring(1)
 }
 
-function Get-SoftwareUri
-{
+function Get-SoftwareUri {
     [CmdletBinding()]
     param(
         [Version] $Version
     )
 
-    if ($null -eq $Version)
-    {
+    if ($null -eq $Version) {
         # Default to latest stable version
         $release = (Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Latest)[0]
     }
-    else 
-    {
+    else {
         $release = Get-GitHubRelease -OwnerName $owner -RepositoryName $repository -Tag "v$($Version.ToString())"
     }
     $releaseAssets = Get-GitHubReleaseAsset -OwnerName $owner -RepositoryName $repository -Release $release.ID
 
     $windowsArchiveAsset = $null
-    foreach ($asset in $releaseAssets)
-    {
-        if ($asset.name -match $installerFileNameRegex)
-        {
+    foreach ($asset in $releaseAssets) {
+        if ($asset.name -match $installerFileNameRegex) {
             $windowsArchiveAsset = $asset
-            break;
+            break
         }
         else {
-            continue;
+            continue
         }
     }
 
-    if ($null -eq $windowsArchiveAsset)
-    {
-        throw "Cannot find published Windows archive asset!"
+    if ($null -eq $windowsArchiveAsset) {
+        throw 'Cannot find published Windows archive asset!'
     }
 
     return $windowsArchiveAsset.browser_download_url
